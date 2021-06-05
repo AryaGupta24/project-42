d193c1ed446f881855089f9c0a04de7851a9c986
Arya Gupta <71432307+AryaGupta24@users.noreply.github.com>
2021-06-05 16:51:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+hr = hour();
+min = minute();
+sec = second();
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
